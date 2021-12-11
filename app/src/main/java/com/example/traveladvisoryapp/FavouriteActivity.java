972c17f93ca00a4ec28e679a77f782aaa83922ee
lindamol <lindamoljosem@gmail.com>
2021-12-11 13:22:43 -0500

    Version2 with Swipe to DeleteAdded

--- app/src/main/java/com/example/traveladvisoryapp/FavouriteActivity.java
+++ app/src/main/java/com/example/traveladvisoryapp/FavouriteActivity.java
@@ -1,11 +1,13 @@
 package com.example.traveladvisoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class FavouriteActivity extends AppCompatActivity implements DataBaseServ
     RecyclerView favorite_Recyclerview;
     ArrayList<Country> listofcountries= new ArrayList<>(0);
     FavouriteAdapter favadapter;
+    ArrayList<Country> dbCountrylist = new ArrayList<>(0);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +37,9 @@ public class FavouriteActivity extends AppCompatActivity implements DataBaseServ
          dbService.getAllCountries();
         //dbService.deleteCountryname("India");
         //dbService.deleteallRows();
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(favorite_Recyclerview);
+
 
 
     }
@@ -41,6 +47,7 @@ public class FavouriteActivity extends AppCompatActivity implements DataBaseServ
     @Override
     public void databaseAllCountriesListener(List<Country> list) {
         listofcountries = new ArrayList<>(list);
+        dbCountrylist = new ArrayList<>(list);
         favadapter.countryList = listofcountries;
         favadapter.notifyDataSetChanged();
         // System.out.println("These are my countryes in DBBBBB" + listofcountries.get(2).countryCode);
@@ -49,13 +56,33 @@ public class FavouriteActivity extends AppCompatActivity implements DataBaseServ
 
     @Override
     public void favcountryClicked(Country favselectedCountry) {
-        dbService.deleteCountryname(favselectedCountry.countryName);
-        favadapter.notifyDataSetChanged();
+//        dbService.deleteCountryname(favselectedCountry.countryName);
+//        favadapter.notifyDataSetChanged();
     }
 
     @Override
     public void codeforFavCountry(String favcountryCode) {
         System.out.println("From Fav Activityyyyy" + favcountryCode);
-
     }
+    //Swipe to delete
+    ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.DOWN, ItemTouchHelper.LEFT |
+            ItemTouchHelper.RIGHT |
+            ItemTouchHelper.DOWN |
+            ItemTouchHelper.UP) {
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            Toast.makeText(FavouriteActivity.this, "Item Moveing", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+            //Remove swiped item from list and notify the RecyclerView
+            int position = viewHolder.getAdapterPosition();
+              dbService.deleteCountryname(dbCountrylist.get(position).getCountryName());
+            favadapter.countryList.remove(position);
+        }
+    };
+
 }
